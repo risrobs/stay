9b7cf3827952fd2e2b3b09632b2779b3858c5f8d
risrobs <robertivanov22@abv.bg>
2021-12-15 02:42:57 +0200

    a

--- src/js/Application.js
+++ src/js/Application.js
@@ -10,16 +10,19 @@ export default class Application extends EventEmitter {
 
   constructor() {
     super();
-
+    this._beat = new Beat();
     const lyrics = ["Ah", "ha", "ha", "ha", "stayin' alive", "stayin' alive"];
     let count = 0;
 
+    this._beat.on(Beat.events.BIT, () => this._create(lyrics[count > lyrics.length - 1 ? count = 0 : count++]))
+    this.emit(Application.events.READY);
+  }
+
+  _create(msg) {
     const message = document.createElement("div");
     message.classList.add("message");
-    message.innerText = "Ah";
+    message.innerText = msg;
 
     document.querySelector(".main").appendChild(message);
-
-    this.emit(Application.events.READY);
   }
 }
